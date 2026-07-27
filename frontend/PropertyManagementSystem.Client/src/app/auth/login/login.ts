0b7980ee432e088ee52612fefa00d8848419d962
Eric Zimmer <contact@ejzimmer.com>
2026-07-27 13:36:34 -0500

    2026-07-27 1336

--- frontend/PropertyManagementSystem.Client/src/app/auth/login/login.ts
+++ frontend/PropertyManagementSystem.Client/src/app/auth/login/login.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { LoginRequest } from './login-request.interface';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,8 @@ export class Login {
   private router = inject(Router);
   private cd = inject(ChangeDetectorRef);
   private authService = inject(AuthService);
+  private http = inject(HttpClient);
+  private document = inject(DOCUMENT);
 
   loginForm = new FormGroup({
     email: new FormControl(''),
@@ -36,6 +40,19 @@ export class Login {
         this.errorMessage = err.message || 'Login failed.';
         this.cd.detectChanges();
       },
+    });
+  }
+
+  googleLoginSubmit() {
+    console.log('Google login clicked.');
+    var url = 'http://localhost:5093/api/auth/signin-google';
+
+    this.http.get<{ authURL: string }>(url).subscribe({
+      next: (data) => {
+        // Redirect the entire browser to the Google OAuth URL
+        this.document.location.href = data.authURL;
+      },
+      error: (err) => console.error('OAuth redirect failed', err),
     });
   }
 }
